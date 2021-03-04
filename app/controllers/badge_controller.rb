b3e23206c51acd7cf71f4565176c6889fd886306
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2021-03-04 08:27:28 +0530

    Update badge_controller.rb

--- app/controllers/badge_controller.rb
+++ app/controllers/badge_controller.rb
@@ -1,9 +1,32 @@
 class BadgeController < ApplicationController
   
+  private def badge_params
+    params.require(:badge).permit(:badge_id, :badge_title, :badge_desc, :badge_img, :created_at, :awarded_at, :revoked_at, :admin_award)
+  end
+  
+  
+  
+  
+  
   def index
   end
   
+  def find_badge
+    @Badge = Badge.find_by(name: params[:badge_id])
+    
+    unless @Badge
+      raise ActiveRecord::RecordNotFound, "Couldn't find badge '#{params[:badge_id]}'"
+    end
+  end
+  
   def create_badge
+    @Badge = Badge.new(badge_params)
+    if @Badge.save 
+      flash[:notice] = ts('New badge successfully created.') 
+    end
+  end
+  
+  def edit_badge
   end
   
   def add_badge

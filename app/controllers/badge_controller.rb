a2eb6d386e080d846b655d595f22a41638a170e1
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2021-03-04 08:16:28 +0530

    Create badge_controller.rb
    added boilerplate template code

---
+++ app/controllers/badge_controller.rb
@@ -0,0 +1,26 @@
+class BadgeController < ApplicationController
+  
+  def index
+  end
+  
+  def create_badge
+  end
+  
+  def add_badge
+  end
+  
+  def revoke_badge
+  end
+    
+  def system_revoke_badge
+  end
+  
+  def hide_badge
+  end
+  
+  def unhide_badge
+  end
+  
+  def check_if_badge_secret
+  end
+end
